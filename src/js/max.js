0f561e281a1615ca8df8ab4f2254534747f94d7b
mxmkhv <maxkehayov@gmail.com>
2017-05-29 19:33:20 +0300

    ITS ALIIIIV!

--- src/js/max.js
+++ src/js/max.js
@@ -8,3 +8,13 @@ function sumThisShit(sum) {
 
 sumThisShit(dummyObject.workWithThis[2].reachMe);
 
+
+function smqtaaaai(pet) {
+	if (pet==5) {
+		console.log('pet')
+	} else if (pet>5) {
+		console.log ('po golqmo ot pet')
+	} else {console.log ('po malko ot pet')}
+};
+
+smqtaaaai(6);
