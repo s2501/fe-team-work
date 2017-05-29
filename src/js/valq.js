ad2fea3ce491b4a0423bcf6e1f48ca93bf42bd85
adored44 <valentina.kireva>
2017-05-29 19:40:27 +0300

    fix typo

--- src/js/valq.js
+++ src/js/valq.js
@@ -1,8 +1,8 @@
 function biggerOrSmaller(n) {
 
-    if (n == 5) {console.log("the number is equal to 5")}
-    else if (n > 5)  {console.log("the is number bigger than 5")}
-    	else {console.log("the is number smaller than 5")}
+    if (n == 5) {console.log("the number is equal to 5");}
+    else if (n > 5)  {console.log("the number is bigger than 5");}
+    	else {console.log("the number is smaller than 5");}
 }
 
 biggerOrSmaller(6);
